7cd5b80d6c1d92201bcbe004999e7e12697db35b
sunyiteng <sunyiteng@bytedance.com>
2025-03-17 19:31:41 +0800

    chore: update

--- test/named-export/rsbuild.config.ts
+++ test/named-export/rsbuild.config.ts
@@ -1,8 +1,15 @@
 import { pluginLess } from '@rsbuild/plugin-less';
 import { pluginSass } from '@rsbuild/plugin-sass';
 import { pluginTypedCSSModules } from '@rsbuild/plugin-typed-css-modules';
+import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
+import { defineConfig } from '@rsbuild/core';
 
-export default {
-  plugins: [pluginLess(), pluginSass(), pluginTypedCSSModules()],
-  output: {},
-};
+
+export default defineConfig({
+  plugins: [pluginLess(), pluginSass(), pluginTypedCSSModules(), pluginTypeCheck()],
+  output: {
+    cssModules: {
+      namedExport: true,
+    }
+  },
+});
---
+++ test/named-export/src/index.ts
@@ -0,0 +1,5 @@
+import { btn } from './b.module.scss';
+import { theCClass } from './c.module.less';
+
+btn;
+theCClass;
